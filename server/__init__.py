d1cb0a1af6b553d1e1a1f0af95b68ee54e3e659b
davidw1339 <davidw1339@gmail.com>
2017-02-25 02:21:04 -0600

    Finished register and get routes

--- server/__init__.py
+++ server/__init__.py
@@ -1,5 +1,6 @@
-from flask import Flask
+from flask import Flask, request
 from pymongo import MongoClient
+import json
 import os
 
 #initialize the app
@@ -18,6 +19,47 @@ db = client.drdot
 @app.route("/")
 def hello():
     return "Welcome to Dr. Dot an integrative suite of features for health and wellness!"
+
+@app.route("/register_user", methods=['POST'])
+def register_user():
+    user_id = request.form['userid']
+    cursor = db.users.find({'userid': user_id})
+    if cursor:
+        for user in cursor:
+            return 'already-registered'
+
+    #get credentials
+    address = request.form['address']
+    name = request.form['name']
+    assistant_phone = request.form['assistantphone']
+    emergency_phone = request.form['emergencyphone']
+
+    user = db.users.insert_one(
+    {
+        "userid": user_id,
+        "address": address,
+        "assistantphone": assistant_phone,
+        "emergencyphone" : emergency_phone,
+    })
+    if user:
+        return "registered"
+    else:
+        return "register-fail"
+
+@app.route("/get_user", methods=['GET'])
+def get_user():
+    userid = request.args.get('userid')
+    user = db.users.find_one({"userid": userid})
+    if user:
+        user_json = {
+            "userid": userid,
+            "address": user["address"],
+            "assistantphone": user["assistantphone"],
+            "emergencyphone": user["emergencyphone"]
+        }
+    else:
+        return "no user found"
+    return json.dumps(user_json)
 
 if __name__ == "__main__":
     app.run(debug=True)
