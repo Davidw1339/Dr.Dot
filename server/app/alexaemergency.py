850ac28946988cfc486da9df8d3b981592eda7e0
davidw1339 <davidw1339@gmail.com>
2017-02-25 11:44:41 -0600

    finished flask ask

--- server/app/alexaemergency.py
+++ server/app/alexaemergency.py
@@ -1,8 +1,27 @@
+from flask import Flask, request, render_template
 from flask_ask import Ask, statement, question, session
 from app import app
 import logging
 
-
 #setup flask app
 ask = Ask(app, "/")
 logging.getLogger("flask_ask").setLevel(logging.DEBUG)
+
+@ask.launch
+def new_game():
+    confirm_msg = render_template('confirm')
+    return question(confirm_msg)
+
+
+@ask.intent("YesIntent")
+def send_msg():
+    #change data to
+    sent_msg = render_template('sent')
+    return statement(sent_msg)
+
+
+@ask.intent("NoIntent")
+def no_request():
+    canceled_msg = render_template('cancel')
+    #set database to cancel emergency
+    return statement(canceled_msg)
