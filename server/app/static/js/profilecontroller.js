4bffed8c8d811d885d92cdced350981b2d04d025
Aditya Chitnis <addster1018@gmail.com>
2017-02-26 01:31:09 -0600

    added functions

--- server/app/static/js/profilecontroller.js
+++ server/app/static/js/profilecontroller.js
@@ -11,4 +11,14 @@ angular.module('myApp.profileview', ['ngRoute'])
 
 .controller('ProfileController', ['$scope', '$location', '$route', '$http', 'authentication', function($scope, $location, $route, $http, authentication) {
   $("#profileview").show();
+
+  $scope.openNav = function() {
+      document.getElementById("mySidenav").style.width = "250px";
+      document.getElementById("main").style.marginLeft = "250px";
+  }
+
+  $scope.closeNav = function() {
+      document.getElementById("mySidenav").style.width = "0";
+      document.getElementById("main").style.marginLeft= "0";
+  }
 }]);
